1f14cc00176e702f8aa26e127a81268d8198951b
roqkfwk1 <sjlee052@gmail.com>
2025-07-02 01:30:16 +0900

    SIZZ API

---
+++ src/main/java/sizz/api/SizzApiApplication.java
@@ -0,0 +1,13 @@
+package sizz.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SizzApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SizzApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sizz/api/SizzApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sizz.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SizzApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
