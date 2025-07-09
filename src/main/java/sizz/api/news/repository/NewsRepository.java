4c8f122745227ecfa5931b2484acd7d736960bab
roqkfwk1 <sjlee052@gmail.com>
2025-07-09 23:40:38 +0900

    뉴스 검색 기능 추가

--- src/main/java/sizz/api/news/repository/NewsRepository.java
+++ src/main/java/sizz/api/news/repository/NewsRepository.java
@@ -1,10 +1,13 @@
 package sizz.api.news.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import sizz.api.news.entity.NewsDocument;
 
 public interface NewsRepository extends MongoRepository<NewsDocument, String> {
 
     boolean existsByArticleId(String articleId);
+    Page<NewsDocument> findByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String title, String description, Pageable pageable);
 
 }
