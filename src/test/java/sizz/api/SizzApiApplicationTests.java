ca4b835e90f7137755d75385f95011b641223857
roqkfwk1 <sjlee052@gmail.com>
2025-07-12 23:38:44 +0900

    테스트 환경에서 RedisTemplate mock bean 등록

--- src/test/java/sizz/api/SizzApiApplicationTests.java
+++ src/test/java/sizz/api/SizzApiApplicationTests.java
@@ -2,14 +2,28 @@ package sizz.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.mockito.Mockito.mock;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@Import(SizzApiApplicationTests.MockConfig.class)
 class SizzApiApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+    @TestConfiguration
+    static class MockConfig {
+        @Bean
+        public RedisTemplate<String, Object> redisTemplate() {
+            return mock(RedisTemplate.class);
+        }
+    }
 }
