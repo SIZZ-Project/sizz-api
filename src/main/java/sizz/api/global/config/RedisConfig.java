768b92fd23a59443feb2381aee874d7dc8c46fbd
roqkfwk1 <sjlee052@gmail.com>
2025-07-12 23:12:12 +0900

    test 환경에서 Redis 설정 제외

--- src/main/java/sizz/api/global/config/RedisConfig.java
+++ src/main/java/sizz/api/global/config/RedisConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -12,6 +13,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @RequiredArgsConstructor
+@Profile("!test")
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
