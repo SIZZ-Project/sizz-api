0af6d211c0a5bcb62cc39c696d99533ad377dff6
roqkfwk1 <sjlee052@gmail.com>
2025-08-09 23:33:23 +0900

    NewsDto JSON 매핑 수정으로 Mongo 저장 정상화

--- src/main/java/sizz/api/news/dto/NewsDto.java
+++ src/main/java/sizz/api/news/dto/NewsDto.java
@@ -1,27 +1,33 @@
 package sizz.api.news.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
-@Getter
-@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class NewsDto {
-
+    @JsonProperty("article_id")
     private String articleId;
     private String title;
     private String description;
     private String link;
+    @JsonProperty("image_url")
     private String imageUrl;
+    @JsonProperty("source_id")
     private String sourceId;
+    @JsonProperty("source_name")
     private String sourceName;
     private List<String> keywords;
     private List<String> category;
+
+    @JsonProperty("pubDate")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime pubDate;
-    private Long viewCount;
 
+    private Long viewCount;
 }
