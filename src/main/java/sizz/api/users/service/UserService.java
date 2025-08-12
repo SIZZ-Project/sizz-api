be373071f0bdd6bc3562b7ae5f0202fb57a6bc21
rhehdgus457 <rhehdgus457@naver.com>
2025-08-13 00:41:52 +0900

    @Autowired 제거

--- src/main/java/sizz/api/users/service/UserService.java
+++ src/main/java/sizz/api/users/service/UserService.java
@@ -14,8 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
-    
-    @Autowired
+
     private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
     private final PasswordEncoder passwordEncoder;
