fd9e3e4443ebd274c3369a10ec330feb8d0d8b5b
roqkfwk1 <sjlee052@gmail.com>
2025-09-09 20:18:07 +0900

    댓글/좋아요/북마크 엔티티 매핑 오류 수정

--- src/main/java/sizz/api/bookmarks/entity/BookmarksEntity.java
+++ src/main/java/sizz/api/bookmarks/entity/BookmarksEntity.java
@@ -18,7 +18,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public class BookmarksEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
 
     @Column(name = "user_id", nullable = false)
     private String userId;
