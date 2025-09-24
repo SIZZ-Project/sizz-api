459a443706774c785748c8c173ec0db187751a0d
rhehdgus457 <rhehdgus457@naver.com>
2025-09-24 21:14:17 +0900

    게시판 common & exception 부분 디폴트 값으로 설정 커밋

--- src/main/java/sizz/api/newsboard/common/ApiResponse.java
+++ src/main/java/sizz/api/newsboard/common/ApiResponse.java
@@ -1,4 +1,41 @@
 package sizz.api.newsboard.common;
 
-public class ApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ApiResponse<T> {
+    private boolean success;  
+    private String message;
+    private T data;                 //제네릭 함수 테스트 및 수정 필요
+
+
+    public static <T> ApiResponse<T> success(T data) {
+        return ApiResponse.<T>builder()
+                .success(true)
+                .message("요청 성공")
+                .data(data)
+                .build();
+    }
+
+
+    public static <T> ApiResponse<T> successMessage(String message) {
+        return ApiResponse.<T>builder()
+                .success(true)
+                .message(message)
+                .build();
+    }
+
+
+    public static <T> ApiResponse<T> fail(String message) {
+        return ApiResponse.<T>builder()
+                .success(false)
+                .message(message)
+                .build();
+    }
 }
